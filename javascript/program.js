956aa1fc08e83d746466bd6507260d96be83ab10
Amir Rajan <ar@amirrajan.net>
2018-05-20 16:47:47 -0400

    [Javascript] Given the input 'FFB', the rover should be at x = 0, y = 1.

--- javascript/program.js
+++ javascript/program.js
@@ -3,14 +3,23 @@
 var _ = require('underscore');
 
 function move(rover, instructions) {
-
+  rover.instructions = instructions.split('');
+  _.each(rover.instructions, function(c) {
+    if (c == 'F' && rover.direction == 'N') {
+      rover.y += 1;
+    } else if (c == 'B' && rover.direction == 'N') {
+      rover.y -= 1;
+    }
+  });
 }
 
 var rover = {
   x: 0,
-  y: 0
+  y: 0,
+  direction: 'N',
+  instructions: []
 };
 
-move({ }, 'FFB');
+move(rover, 'FFB');
 
 console.log(rover);
